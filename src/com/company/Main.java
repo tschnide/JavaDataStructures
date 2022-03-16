1cca6c7b21bcf22407492c14cf697fa0dfdac1b5
tschnide <travis.schnider@gmail.com>
2022-03-16 00:27:55 -0600

    Create resizable array class.

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ResizeableArray arr = new ResizeableArray(5);
+        arr.GetSize();
+    }
+}
---
+++ src/com/company/ResizeableArray.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+public class ResizeableArray {
+    private int size;
+    private int[] arr = new int[size];
+
+    ResizeableArray(int size){
+        this.size = size;
+    }
+
+
+}

4307d1c2f76e2cd346e51b26398c5b223b3fced8
Travis Schnider <travis.schnider@gmail.com>
2022-03-16 01:11:28 -0600

    Add insert method.
    An insert method that resizes when the array is full.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        ResizeableArray arr = new ResizeableArray(5);
-        arr.GetSize();
+        ResizeableArray arr = new ResizeableArray(2);
+        arr.insert(10);
+        arr.insert(10);
+        arr.insert(10);
+        arr.insert(10);
+
+        System.out.println(arr);
     }
 }
--- src/com/company/ResizeableArray.java
+++ src/com/company/ResizeableArray.java
@@ -3,11 +3,22 @@ package com.company;
 
 public class ResizeableArray {
     private int size;
-    private int[] arr = new int[size];
+    private int objectsStored = 0;
+    private int[] array;
 
     ResizeableArray(int size){
-        this.size = size;
+        array = new int[size];
     }
 
+    public void insert(int numToInsert){
+        if(array.length <= objectsStored) {
+            int[] tempArray = array.clone();
+            array = new int[array.length*2];
+            System.arraycopy(tempArray,0, array, 0, tempArray.length);
+        }
+        int insertIndex = objectsStored;
+        objectsStored++;
+        array[insertIndex] = numToInsert;
+    }
 
 }

ae8691e0f6ee752b32b03f1e8d79ee710c343e97
tschnide <travis.schnider@gmail.com>
2022-03-16 01:25:12 -0600

    Annotate new work.

--- src/com/company/ResizeableArray.java
+++ src/com/company/ResizeableArray.java
@@ -1,6 +1,10 @@
 package com.company;
 
-
+/**
+ * An array with extended capabilities including:
+ * automatic resizing (up and down), removal of elements
+ * at specified indices, and finding a specified index value.
+ */
 public class ResizeableArray {
     private int size;
     private int objectsStored = 0;
@@ -10,6 +14,10 @@ public class ResizeableArray {
         array = new int[size];
     }
 
+    /**
+     * Inserts at the end of the array.
+     * @param numToInsert - The number to insert.
+     */
     public void insert(int numToInsert){
         if(array.length <= objectsStored) {
             int[] tempArray = array.clone();
